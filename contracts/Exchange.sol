5cfbd0510386ab9b5df21ac29828d540d9cfa7a9
Mac Studio <macstudio@Macs-Mac-Studio.local>
2022-12-05 10:35:07 -0500

    10. Withdraw tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
@@ -10,6 +10,12 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -17,18 +23,32 @@ contract Exchange {
     }
 
 
-    // ---------------------------
-    // DEPOSIT & WITHDRAW TOKEN 
+    // ------------------------
+    // DEPOSIT & WITHDRAW TOKEN
 
     function depositToken(address _token, uint256 _amount) public {
-    	// Transfer tokens to exchange
-    	require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        // Transfer tokens to exchange
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
 
-    	// Update user balance
-    	tokens[_token][msg.sender] = tokens [_token][msg.sender] + _amount;
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
 
-    	// Emit an event
-    	emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+        // Emit an event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        // Ensure user has enough tokens to withdraw
+        require(tokens[_token][msg.sender] >= _amount);
+
+        // Transfer tokens to user
+        Token(_token).transfer(msg.sender, _amount);
+
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        // Emit event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     function balanceOf(address _token, address _user)
@@ -36,7 +56,7 @@ contract Exchange {
         view
         returns (uint256)
     {
-    	return tokens [_token][_user];
+        return tokens[_token][_user];
     }
 
 }
