c26ccf9f84d428f0fbcc80a1082970bc8dad991a
Mac Studio <macstudio@Macs-Mac-Studio.local>
2022-12-08 12:58:58 -0500

    14. Seed exchange

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,20 +1,20 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract Token {
-	string public name;
-	string public symbol;
-	uint256 public decimals = 18;
-	uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
-        address indexed from, 
-        address indexed to, 
+        address indexed from,
+        address indexed to,
         uint256 value
     );
 
@@ -25,19 +25,19 @@ contract Token {
     );
 
     constructor(
-    	string memory _name, 
-    	string memory _symbol, 
-    	uint256 _totalSupply
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
     ) {
-    	name = _name;
-    	symbol = _symbol;
-    	totalSupply = _totalSupply * (10**decimals);  // 1,000,000 x 10^18;
-    	balanceOf[msg.sender] = totalSupply;
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) 
-        public 
-        returns (bool success) 
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
 
@@ -59,8 +59,8 @@ contract Token {
         emit Transfer(_from, _to, _value);
     }
 
-    function approve(address _spender, uint256 _value) 
-        public 
+    function approve(address _spender, uint256 _value)
+        public
         returns(bool success)
     {
         require(_spender != address(0));
@@ -73,7 +73,7 @@ contract Token {
 
     function transferFrom(
         address _from,
-        address _to, 
+        address _to,
         uint256 _value
     )
         public
@@ -81,10 +81,10 @@ contract Token {
     {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
-        
+
         allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
-        _transfer (_from, _to, _value);
+        _transfer(_from, _to, _value);
 
         return true;
     }
